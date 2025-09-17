566c769469e6c0a9e678a59424cf97867f63e5dc
Himansh1009 <2370270@student.bic.edu.np>
2025-09-17 11:02:00 +0545

    new 3

---
+++ meaning.py
@@ -0,0 +1,10 @@
+meaning = 8
+print('')
+
+# if meanning > 10:
+#     print('Right on!')
+# else:
+#     print('Not Today!')
+
+# Ternary operator
+print('Right on!' if meaning > 10 else 'Not Today!')
