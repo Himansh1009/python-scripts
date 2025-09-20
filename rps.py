a4c3998a07345ded159d9bbc31c2a88f56f0cb82
Himansh1009 <2370270@student.bic.edu.np>
2025-09-20 23:02:50 +0545

    new add 10

--- rps.py
+++ rps.py
@@ -14,7 +14,7 @@ playerchoice = input("Enter...\n1 for Rock,\n2 for Paper,  or \n3 for Scissor:\n
 
 player = int(playerchoice)
 
-if player < 1 | player > 3:
+if player < 1 or player > 3:
     sys.exit("You must enter 1, 2, or 3.")
 
 computerchoice = random.choice("123")
@@ -22,8 +22,8 @@ computerchoice = random.choice("123")
 computer = int(computerchoice)
 
 print("")
-print("You chose " + str(RPS(player)) + ".")
-print("Python chose " + str(RPS(computer)) + ".")
+print("You chose " + str(RPS(player)).replace('RPS.','') + ".")
+print("Python chose " + str(RPS(computer)).replace('RPS.','') + ".")
 print("")
 
 if player == 1 and computer == 3:
